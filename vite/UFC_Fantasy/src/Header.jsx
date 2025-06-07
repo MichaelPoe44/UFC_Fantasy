f67db7eec283fb3484abad21a74714091a1f1e33
MichaelPoe44 <michaelcp387@gmail.com>
2025-06-07 17:04:26 -0500

    new

--- vite/UFC_Fantasy/src/Header.jsx
+++ vite/UFC_Fantasy/src/Header.jsx
@@ -4,8 +4,11 @@ import {Link} from "react-router-dom"
 export default function Header(){
     return(
         <div className="header">
-        
+            {/*temp linkage*/}
+            <Link to="/">
             <p className="header_text" id="drop_down_button">Three dots here</p>
+            </Link>
+            
             <Link to="/profile">
                 <p className="header_text" id="profile_button">Profile icon</p>
             </Link>
--- vite/UFC_Fantasy/src/pages/Profile.jsx
+++ vite/UFC_Fantasy/src/pages/Profile.jsx
@@ -1,14 +1,29 @@
 
+{/*async since having to fetch*/}
+const get_data = async () => {         //my flask endpoint
+    const res = await fetch("http://127.0.0.1:5000/incomes");
+    const data = await res.json();
+    return data;
+}
+
+const get_info = async () => {
+    const temp = await get_data();
+    console.log(temp);
+    console.log("temp ^^^^^^^^");
+    return temp;
+}
 
 
 export default function Profile({team, change_team}){
+    const info = get_info();
+
     return(
         <div className="profile">
         
         <button onClick={() => console.log(team)}>this</button>
         
         
-        
+        <button onClick={() => console.log(info)}>Flask info</button>
         </div>
     )
 }
